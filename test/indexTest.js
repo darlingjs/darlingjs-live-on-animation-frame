2fc5da175e03b6451d0390c525ce068190413e72
Eugene Krevenets <hyzhak@ya.ru>
2015-07-13 15:43:02 +0200

    setup travis CI

---
+++ test/indexTest.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var runner = require('../');
+var expect = require('chai').expect;
+
+describe('live on animation frame', function() {
+  it('should return function', function() {
+    expect(runner()).to.be.a('function');
+  });
+});
